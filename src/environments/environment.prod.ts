6ac906cfc13c224917acc830917a590a96a12306
Developer <dev@local>
2026-02-20 22:22:01 +0200

    stage9

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // Update these values for your production environment
   // You can also use environment variables during build time
-  apiBaseUrl: 'https://aegho.us-east-1.elasticbeanstalk.com:5000',
-  frontendBaseUrl: 'https://aegho.us-east-1.elasticbeanstalk.com:5000'
+  apiBaseUrl: 'http://aegho.us-east-1.elasticbeanstalk.com:5000',
+  frontendBaseUrl: 'http://aegho.us-east-1.elasticbeanstalk.com:5000'
 };
