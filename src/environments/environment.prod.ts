85b16eccc0ebad6051956b21d6c0eac2d418abb4
Developer <dev@local>
2026-02-20 22:01:52 +0200

    trx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // Update these values for your production environment
   // You can also use environment variables during build time
-  apiBaseUrl: 'https://api.yourdomain.com',
-  frontendBaseUrl: 'https://yourdomain.com',
+  apiBaseUrl: 'https://aegho.us-east-1.elasticbeanstalk.com:5000',
+  frontendBaseUrl: 'https://aegho.us-east-1.elasticbeanstalk.com:5000'
 };
